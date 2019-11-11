d1b4065911f4abc34c3c47aa6450fd37cb9fd4a1
sameedali <sameed.ali.94@gmail.com>
2019-11-11 13:25:11 -0500

    adding expr prints

--- src/pprint.ml
+++ src/pprint.ml
@@ -3,120 +3,160 @@ open Location
 open Ast
 open Stdlib
 
+let print_string str fmt _ =  string fmt str;;
+
 let pprint_ploc fmt l =
-  let x, y, z = get_pos_info l.loc_start
-  in pf fmt "Location: \n loc_start_pos: %s %d %d" x y z
+    let x, y, z = get_pos_info l.loc_start
+  in pf fmt " Location: \n loc_start_pos: %s %d %d" x y z
 
-let pprint_pelem fmt pelem = "placehilder pelem";;
+let pprint_str_location fmt f =
+    braces
+    (concat ~sep:comma
+    [(fun fmt t ->
+        pf fmt "string: ";
+        string fmt f.pelem);
+     (fun fmt t -> pprint_ploc fmt f.ploc)])
+    fmt f;;
 
-let pprint_location fmt loc = pprint_ploc fmt loc.ploc
-
-(* START *)
-(*
-let pprint_decl_use decl_use =
-  str "decl_use: %s \n" decl_use.use_module.pelem
-
-let pprint_non_term_varname = function
-  | Some x -> Printf.sprintf "non-term-var-name %s\n" x.pelem
-  | None -> "non-term-var-name: empty here"
-
-let pprint_type_expr = function
-  | TE_path p -> "TE apth\n"
-  | _ -> "type expr otherwise\n"
-
-let pprint_param_decl x = "skip-param decl"
-
-  (* (? takes a function) x.pelem.type_expr *)
-
-let rec pprint_non_term_inh_attrs = function
-  | [] -> "EOL/empty non term arrts\n"
-  | hd :: tl -> (pprint_param_decl hd) ^ "-" ^ (pprint_non_term_inh_attrs tl)
-
-let pprint_non_term_syn_attrs a = String.concat "+" (List.map pprint_param_decl a);;
-
-let rec pprint_rule_elem r = pprint_rule_elem_desc r.rule_elem
-and pprint_rule_elem_desc = function
-  | RE_literal a -> Printf.sprintf "%s " a.pelem
-  | RE_seq a -> String.concat "-seq-" (List.map pprint_rule_elem a)
-  | RE_star  a -> pprint_rule_elem a
-  | _ -> "etc rule elem";;
-
-let rec print_rule r = "\nrule:"
-                       ^ (String.concat "+" (List.map pprint_rule_elem r.rule_rhs))
-                       ^ (String.concat "+" (List.map pprint_param_decl r.rule_temps))
-                         ^ "EOR\n"
-
-let pprint_non_term_rules a = String.concat "+" (List.map print_rule a);;
-
-let pprint_decl_non_term a =
-  Printf.sprintf "non-term-decl: %s\n" a.non_term_name.pelem
-  ^ pprint_non_term_varname a.non_term_varname
-  ^ pprint_non_term_inh_attrs a.non_term_inh_attrs
-  ^ pprint_non_term_syn_attrs a.non_term_syn_attrs
-  ^ pprint_non_term_rules a.non_term_rules
-;;
-(* let pprint_format_decl_desc = function
- *   | Decl_use x -> pprint_decl_use x
- *   | Decl_non_term y -> pprint_decl_non_term y
- *   | _ -> "other type" *)
-
-(* END *)
-*)
-
-let pprint_ident fmt f = string fmt f.pelem;;
+let pprint_ident = braces (print_string "ident: " ++ pprint_str_location);;
 
 let pprint_format_name fmt f =
     pf fmt "format_name: ";
     pprint_ident fmt f.format_name;;
 
-let pprint_loc fmt l = pf fmt "loc: xyz" ;;
+let pprint_loc fmt l = pf fmt "loc: _";;
 let pprint_format_loc fmt f = pprint_loc fmt f.format_loc;;
 let pprint_format_decl_loc fmt f = pprint_loc fmt f.format_decl_loc;;
 let pprint_format_lob fmt f = pf fmt "format_lob: xyz" ;;
 
-let pprint_non_term_name fmt non_term_defn = pprint_ident fmt non_term_defn.non_term_name
+let pprint_non_term_name fmt non_term_defn = pf fmt "non_term_name: "; pprint_ident fmt non_term_defn.non_term_name
 
 let pprint_non_term_varname fmt non_term_defn =
     match non_term_defn.non_term_varname with
     | Some varname -> pf fmt "varname: "; pprint_ident fmt varname;
     | None -> pf fmt "varname: "; pf fmt "none";;
 
-let pprint_str_location fmt x = pf fmt "placeholder str_loc";;
+let pprint_path = brackets (list pprint_ident);;
 
-let pprint_expr fmt x = pf fmt "placeholder_expr";;
-let pprint_rule_constraint fmt x = pprint_expr fmt x.expr
+let rec pprint_expr_desc fmt = function
+  | E_path x -> pf fmt "E_path:"; brackets pprint_path fmt x
+  | E_int x -> pf fmt "E_int: "; pf fmt "%d" x;
+  | E_tuple x -> braces (list pprint_expr) fmt x
+  | E_apply (x , y) -> pf fmt "E_apply:"; brackets (fun fmt z -> pprint_expr fmt x; braces (list pprint_expr) fmt y) fmt y
+  | E_unop (u, e) -> pf fmt "E_unop"
+  | E_binop (b, e1, e2 ) -> pf fmt "binop * expr * expr"
+  | E_index (e,e1) -> pf fmt "expr * expr"
+  | E_literal x -> pf fmt "E_literal: "; brackets pprint_str_location fmt x
+  | E_cast (e, p) -> pf fmt "E_cast:expr * path"
+  | E_field (e, p) -> pf fmt "E-field: expr * path"
+  | E_case (e, pe) -> pf fmt "E_case: expr * (pattern * expr) list"
+  | E_let (p,e,e1) -> pf fmt "pattern * expr * expr"
+and pprint_expr fmt x =
+  pf fmt "expr: ";
+  brackets
+    (fun fmt t ->
+       pprint_loc fmt t.expr_loc;
+       pprint_expr_desc fmt t.expr)
+    fmt x;;
 
-let pprint_rule_elem_desc fmt = function
-  | RE_literal x -> pprint_str_location fmt x
-  | RE_non_term (x, y, z) -> pf fmt "placeholder"
-  | RE_named_regex (x, y) -> pf fmt "placeholder"
-  | RE_constraint x -> pprint_rule_constraint fmt x
-  | RE_action x -> pf fmt "placeholder"
-  | RE_choice (x, y) -> pf fmt "placeholder"
-  | RE_seq x -> pf fmt "placeholder"
-  | RE_star x -> pf fmt "placeholder"
-  | RE_plus x -> pf fmt "placeholder"
-  | RE_opt x -> pf fmt "placeholder"
-  | RE_repeat (x, y)-> pf fmt "placeholder"
-  | RE_char_class x -> pf fmt "placeholder"
-
-let pprint_rule_elem_loc fmt rule_elem = pprint_loc fmt rule_elem.rule_elem_loc;;
+let pprint_rule_constraint = pprint_expr
 
 let pprint_rule_loc fmt rule_loc = pf fmt "rule loc placeholder";;
 
-let pprint_rule_temps fmt x = pf fmt "asdasd";;
+let pprint_rule_temps fmt x = pf fmt "rule_tmps";;
 
-let pprint_rule_elem fmt rule_elem = pf fmt "asdsad";; (*brackets ( pprint_rule_loc ++ pprint_rule_temps ++ pprint_rule_elem_desc);;
-*)
-let pprint_rule fmt rule =
-    pf fmt "rule rhs:";
-    brackets (list pprint_rule_elem) fmt rule.rule_rhs;;
+let pprint_stmt_desc fmt = function
+    | S_assign (expr1, expr2) ->
+        pf fmt "S assign: ";
+        pprint_expr fmt expr1;
+        pprint_expr fmt expr2;;
+let pprint_stmt fmt t = pprint_stmt_desc fmt t.stmt
+
+let pprint_rule_action fmt t =
+    pprint_loc fmt t.action_loc ;
+    pf fmt  ", ";
+    (brackets (list pprint_stmt)) fmt t.action_stmts;;
+
+let rec pprint_rule_elem fmt rule_elem =
+    pf fmt "rule elem:";
+  braces
+  (concat ~sep:comma
+  [ (fun fmt t -> pprint_loc fmt t.rule_elem_loc);
+    (fun fmt t -> pprint_rule_elem_desc fmt t.rule_elem) ])
+  fmt rule_elem
+  and pprint_rule_elem_desc fmt = function
+      | RE_literal x -> pf fmt "RE_literal: "; pprint_str_location fmt x
+      | RE_non_term (x, y, z) -> pf fmt "RE_nonterm_placeholder"
+      | RE_named_regex (rule_elem, ident) ->
+          braces
+          (fun fmt t ->
+              pprint_rule_elem fmt rule_elem;
+              pprint_ident fmt ident;) fmt fmt;
+      | RE_constraint x -> pprint_rule_constraint fmt x
+      | RE_action x -> pf fmt "RE_action: "; pprint_rule_action fmt x
+      | RE_choice (x, y) -> pf fmt "placeholder choice"
+      | RE_seq x -> pf fmt "placeholder seq"
+      | RE_star x -> pf fmt "placeholder star"
+      | RE_plus x -> pf fmt "placeholder plus"
+      | RE_opt x -> pf fmt "placeholder opt"
+      | RE_repeat (x, y)-> pf fmt "placeholder repeat"
+      | RE_char_class x -> pf fmt "placeholder chear class"
+;;
+
+
+let pprint_tvar fmt t = pf fmt "tvar: "; pprint_str_location fmt t;;
+
+let rec pprint_type_expr_desc fmt = function
+    | TE_tvar x -> pf fmt "TE_tvar: "; pprint_tvar fmt x
+  | TE_path x -> pf fmt "TE_path: ";  pprint_path fmt x
+  | TE_tuple x -> pf fmt "TE_tuple: "; brackets (list pprint_type_expr) fmt x
+  | TE_list x -> pf fmt "TE_list: "; pprint_type_expr fmt x
+  | TE_constr (x, y) ->
+          pf fmt "TE_constr: ";
+          pprint_path fmt x;
+          brackets (list pprint_type_expr) fmt y;
+  | TE_app (x, y) ->
+          pprint_path fmt x;
+          brackets (list pprint_type_expr) fmt y
+      and pprint_type_expr fmt x =
+          braces
+    (fun fmt t ->
+        pprint_type_expr_desc fmt t.type_expr;
+        pf fmt ", ";
+        pprint_loc fmt t.type_expr_loc;
+          ) fmt x;;
+
+let pprint_param_decl =
+    print_string "pram decl:"
+     ++
+    braces
+    (fun fmt param_decl ->
+        let ident, type_expr = param_decl.pelem in
+        pprint_ident fmt ident;
+        pf fmt ",";
+        pprint_type_expr fmt type_expr);;
+
+let pprint_rule_temps = print_string "rule temps: " ++ brackets (list pprint_param_decl);;
+
+let pprint_rule =
+    braces
+  (concat ~sep:comma
+    [(fun fmt t ->
+        pf fmt "rule rhs:";
+        brackets (list pprint_rule_elem) fmt t.rule_rhs;);
+    (fun fmt t ->
+        pf fmt "rule temps: ";
+        pprint_rule_temps fmt t.rule_temps);
+    (fun fmt t ->
+        pf fmt "rule loc: ";
+        pprint_loc fmt t.rule_loc)
+  ])
+;;
 
 let pprint_non_term_inh_attrs fmt non_term_defn =
     pf fmt "non_term_rules:";
     brackets (list pprint_rule) fmt non_term_defn.non_term_rules;;
 
-let pprint_param_decl fmt param_decl = pf fmt "skip-param-decl"
 
 let pprint_non_term_inh_attrs fmt non_term_defn =
     pf fmt "non_term_inh_attrs: ";
@@ -126,17 +166,28 @@ let pprint_non_term_syn_attrs fmt non_term_defn =
     pf fmt "non_term_syn_attrs: ";
     brackets (list pprint_param_decl) fmt non_term_defn.non_term_syn_attrs;;
 
-let pprint_non_term_defn fmt non_term_defn =
-    pf fmt "non_term_defn: " ;
-    braces (concat ~sep:comma [ pprint_non_term_name; pprint_non_term_varname; pprint_non_term_inh_attrs; pprint_non_term_syn_attrs; ]) fmt non_term_defn;;
+let pprint_non_term_rules fmt t = (brackets (list pprint_rule)) fmt t.non_term_rules;;
+let pprint_non_term_loc fmt t = pprint_loc fmt t.non_term_loc;;
+
+let pprint_non_term_defn =
+    print_string "non_term_defn: " ++
+    braces
+    (concat ~sep:comma
+    [ pprint_non_term_name;
+      pprint_non_term_varname;
+      pprint_non_term_inh_attrs;
+      pprint_non_term_syn_attrs;
+      pprint_non_term_rules;
+      pprint_non_term_loc]);;
 
 let pprint_format_decl_desc fmt f =
     match f.format_decl with
     | Format_decl_non_term x -> pprint_non_term_defn fmt x
 
-let pprint_format_decl fmt format_decl =
-    pf fmt "format_decl: ";
-    braces (concat ~sep:comma [pprint_format_decl_desc; pprint_format_decl_loc]) fmt format_decl;;
+let pprint_format_decl =
+    print_string "format_decl: "
+    ++ (braces (concat ~sep:comma
+    [pprint_format_decl_desc; pprint_format_decl_loc]));;
 
 let pprint_format_decls_list fmt f =
     pf fmt "format_decls_list: ";
