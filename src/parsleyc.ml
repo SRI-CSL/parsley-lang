14bf3057b7668d1718669a87b78032834ef391d1
Prashanth Mundkur <prashanth.mundkur@gmail.com>
2020-11-19 10:49:43 -0800

    Turn off debug trace and type prints; print a simple spec accept message on success.

--- src/parsleyc.ml
+++ src/parsleyc.ml
@@ -1,4 +1,4 @@
-let opt_print_ast = ref true
+let opt_print_ast = ref false
 let opt_type_check = ref true
 let input_file = ref []
 
@@ -17,8 +17,9 @@ let () =
   if List.length !input_file > 1 || List.length !input_file = 0
   then (Printf.eprintf "Please specify a single input file.\n";
         exit 1);
-  let spec = SpecParser.parse_spec (List.hd !input_file) in
+  let spec_file = List.hd !input_file in
+  let spec = SpecParser.parse_spec spec_file in
   if !opt_print_ast then
     AstPrinter.print_program spec;
   if !opt_type_check then
-    SpecTyper.type_check spec
+    SpecTyper.type_check spec_file spec
