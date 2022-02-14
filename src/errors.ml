5f9678e9703312c35542a0d2ba14168f3c2df413
Benjamin J. Cape <benjamin.j.cape.22@dartmouth.edu>
2022-02-14 15:44:19 -0500

    Squashed commit of the following (with minor edits) to support a json-errormsg mode: Closes #13.
    commit 0b3f53d5a11c1aba39606f8c08235e0a4459d50e
    Author: Benjamin J. Cape <benjamin.j.cape.22@dartmouth.edu>
    Date:   Mon Feb 14 13:59:25 2022 -0500
    
        fix field names
    
    commit e2ebdaa13cc88b7298409181d9e1132dcf0bd9e4
    Author: Benjamin J. Cape <benjamin.j.cape.22@dartmouth.edu>
    Date:   Mon Feb 14 13:57:56 2022 -0500
    
        json support

--- src/errors.ml
+++ src/errors.ml
@@ -17,11 +17,30 @@
 
 module Location = Parsing.Location
 
+type errormsg =
+  {errm_start:  Location.pos;
+   errm_end:    Location.pos;
+   errm_ghost:  bool;
+   errm_reason: string}
+    [@@deriving to_yojson];;
+
+let mk_json_errormsg t msg =
+  let err =
+    {errm_start  = Location.position_to_pos (Location.get_start t);
+     errm_end    = Location.position_to_pos (Location.get_end t);
+     errm_ghost  = Location.is_ghost t;
+     errm_reason = msg} in
+  errormsg_to_yojson err
+
 (* `bt`  contains the backtrace if this is a debugging build.
    `loc` contains the location of the error.
    `msg` is the error message from the compiler. *)
 let handle_exception bt loc msg =
-  let content = Location.content_of_loc loc in
-  Printf.printf "%s\n" bt;
-  Printf.fprintf stderr "%s%s: %s\n" content (Location.str_of_loc loc) msg;
-  exit 1
+   if   !Options.json_out
+   then Printf.fprintf stderr "%s" (Yojson.Safe.to_string (mk_json_errormsg loc msg))
+   else (
+      let content = Location.content_of_loc loc in
+      Printf.printf "%s\n" bt;
+      Printf.fprintf stderr "%s%s: %s\n" content (Location.str_of_loc loc) msg;
+   );
+   exit 1
