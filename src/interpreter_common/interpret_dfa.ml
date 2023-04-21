a9f11f718c876aaa436cbbe391428cfaed9e85f6
Prashanth Mundkur <prashanth.mundkur@gmail.com>
2023-04-21 15:06:20 -0400

    Move the generic DFA interpreter to the interpreter_common/ directory.

--- src/interpreter_common/interpret_dfa.ml
+++ src/interpreter_common/interpret_dfa.ml
@@ -19,7 +19,7 @@
    matched and an updated view) if any. *)
 
 open Dfa.Automaton
-open Interpreter_common.Values
+open Values
 
 let run (dfa: DFA.t) (v: view) : (value * view) option =
   let buf   = !(v.vu_buf) in
