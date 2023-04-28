7cc47a55d9194280513b9cbf29eb8a1a1e248a80
Prashanth Mundkur <prashanth.mundkur@gmail.com>
2023-04-28 14:05:55 -0400

    Disable bound check assert.

--- src/anfscf_interpreter/state.ml
+++ src/anfscf_interpreter/state.ml
@@ -54,7 +54,9 @@ module VEnv = struct
     Bindings.mem v t
 
   let remove (t: t) (v: Anf_common.varid) : t =
-    assert (bound t v);
+    (* Variables are re-used across recursive calls, so we cannot
+       assert this (yet). *)
+    (* assert (bound t v); *)
     Bindings.remove v t
 end
 
